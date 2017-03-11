28ceed0a43b96fa240c58197a3becee4926a7dc8
Peter Halasz <peter.halasz@live.com>
2017-03-11 22:09:42 +0100

    Logic from snippet router extracted to controller

--- app/snippets/router.js
+++ app/snippets/router.js
@@ -1,55 +1,8 @@
 var router = require('express').Router();
-var Snippet = require('./snippet-model.js');
+var controller = require('./snippet-controller.js')
 
-function getSnippets(req, res) {
-    Snippet
-        .find()
-        .select('_id name description')
-        .exec(function(err, snippets) {
-            if (err) {
-                console.log('Error while getting snippets');
-                res.status(500);
-                res.end('Error while getting snippets');
-            } else {
-                res.status(200);
-                res.end(JSON.stringify(snippets));
-            }
-        });
-}
-
-function getSnippet(req, res) {
-    Snippet
-        .findById(req.params.id)
-        .select('name description code')
-        .exec(function(err, snippet) {
-            if (err) {
-                console.log('Error while getting snippet id: ' + req.params.id);
-                res.status(500);
-                res.end('Error while getting snippet id: ' + req.params.id);
-            } else {
-                res.status(200);
-                res.end(JSON.stringify(snippet));
-            }
-        });
-}
-
-function createSnippet(req, res) {
-    var newSnippet = new Snippet(req.body);
-
-    newSnippet.save(function(err, snippet) {
-        if (err) {
-            console.log("Error while saving new snippet");
-            res.status(500);
-            res.end('Error while creating snippet');
-        } else {
-            res.status(201);
-            res.end('Snippet: ' + snippet.name + " created");
-        }
-    });
-}
-
-router.get('/snippets', getSnippets);
-router.get('/snippets/:id', getSnippet);
-router.post('/snippets', createSnippet);
+router.get('/snippets', controller.getSnippets);
+router.get('/snippets/:id', controller.getSnippet);
+router.post('/snippets', controller.createSnippet);
 
 module.exports = router;
---
+++ app/snippets/snippet-controller.js
@@ -0,0 +1,50 @@
+var Snippet = require('./snippet-model.js');
+
+module.exports = {
+    getSnippets: function (req, res) {
+        Snippet
+            .find()
+            .select('_id name description')
+            .exec(function(err, snippets) {
+                if (err) {
+                    console.log('Error while getting snippets');
+                    res.status(500);
+                    res.end('Error while getting snippets');
+                } else {
+                    res.status(200);
+                    res.end(JSON.stringify(snippets));
+                }
+            });
+    },
+
+    getSnippet: function (req, res) {
+        Snippet
+            .findById(req.params.id)
+            .select('name description code')
+            .exec(function(err, snippet) {
+                if (err) {
+                    console.log('Error while getting snippet id: ' + req.params.id);
+                    res.status(500);
+                    res.end('Error while getting snippet id: ' + req.params.id);
+                } else {
+                    res.status(200);
+                    res.end(JSON.stringify(snippet));
+                }
+            });
+    },
+
+    createSnippet: function (req, res) {
+        var newSnippet = new Snippet(req.body);
+
+        newSnippet.save(function(err, snippet) {
+            if (err) {
+                console.log("Error while saving new snippet");
+                res.status(500);
+                res.end('Error while creating snippet');
+            } else {
+                res.status(201);
+                res.end('Snippet: ' + snippet.name + " created");
+            }
+        });
+    }
+}
