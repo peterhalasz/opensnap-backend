df1c85410e4da8360128e9dd2605648f93afacfc
Antal Meckl <antal.meckl@sigmatechnology.se>
2017-04-05 13:58:59 +0200

    Add Access-Control-Allow-Origin and frontendUrl setting

--- app/app.js
+++ app/app.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 var snippets = require('./snippets/router.js');
 var config = require('./config.js');
 
 var app = express();
-
-app.use(bodyParser.json())
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', config.frontendUrl);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+app.use(bodyParser.json());
 app.use('/api/snippets', snippets);
 
 app.listen(config.express.port, config.express.ip, function (error) {
--- app/template_config.js
+++ app/template_config.js
@@ -3,10 +3,12 @@ var config = module.exports;
 config.express = {
     port: 8088,
     ip: ''
-}
+};
 
 config.mongo = {
     port: 27017,
     ip: '',
     database: ''
-}
+};
+
+config.frontendUrl = '';
