8f9a8714b1ab4d0c2a61ec82219dc009b7753dc4
Peter Halasz <peter.halasz@live.com>
2017-04-24 00:02:23 +0200

    Update snippet endpoint added

--- app/snippets/router.js
+++ app/snippets/router.js
@@ -4,5 +4,6 @@ var controller = require('./snippet-controller.js')
 router.get('/', controller.getSnippets);
 router.get('/:id', controller.getSnippet);
 router.post('/', controller.createSnippet);
+router.put('/:id', controller.updateSnippet);
 
 module.exports = router;
--- app/snippets/snippet-controller.js
+++ app/snippets/snippet-controller.js
@@ -38,9 +38,35 @@ module.exports = {
     createSnippet: function (req, res) {
         var newSnippet = new Snippet(req.body);
 
+        if (newSnippet._id) {
+            Snippet.findById(newSnippet._id, function(err, snippet) {
+                if (err) {
+                    console.log('Error while finding snippet: ' + err);
+                    res.status(500);
+                    res.end('Error while finding snippet');
+                } else {
+                    snippet.code = req.body.code;
+                    snippet.name = req.body.name;
+                    snippet.description = req.body.description;
+                    snippet.save(function(err) {
+
+                        if (err) {
+                            console.log('Error while updating snippet: ' + err);
+                            res.status(500);
+                            res.end('Error while updating snippet');
+                        }
+
+                        res.setHeader('Content-Type', 'application/json');
+                        res.status(200);
+                        res.end(JSON.stringify(snippet));
+                    });
+                }
+            });
+        }
+
         newSnippet.save(function(err, snippet) {
             if (err) {
-                console.log("Error while saving new snippet");
+                console.log('Error while saving new snippet: ' + err);
                 res.status(500);
                 res.end('Error while creating snippet');
             } else {
@@ -49,5 +75,30 @@ module.exports = {
                 res.end(JSON.stringify(snippet));
             }
         });
+    },
+
+    updateSnippet: function (req, res) {
+        Snippet.findById(req.params.id, function(err, snippet) {
+            if (err) {
+                console.log('Error while finding snippet: ' + err);
+                res.status(500);
+                res.end('Error while finding snippet');
+            } else {
+                snippet.code = req.body.code;
+                snippet.name = req.body.code.name;
+                snippet.description = req.body.code.description;
+                snippet.save(function(err) {
+                    if (err) {
+                        console.log('Error while updating snippet: ' + err);
+                        res.status(500);
+                        res.end('Error while updating snippet');
+                    }
+
+                    res.setHeader('Content-Type', 'application/json');
+                    res.status(200);
+                    res.end(JSON.stringify(snippet));
+                });
+            }
+         });
     }
 }
